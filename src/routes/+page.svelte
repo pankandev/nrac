d33c5f3c9a924840edb709295ab691502f014bdf
capaths <pcampaa@gmail.com>
2023-02-10 03:40:40 -0300

    refactor: replaced angular with svelte and tauri

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Counter from './Counter.svelte';
+</script>
+
+<svelte:head>
+    <title>Home</title>
+    <meta name="description" content="Svelte demo app"/>
+</svelte:head>
+
+<section>
+    <div class="flex">
+        <Counter/>
+    </div>
+</section>
